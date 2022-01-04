116bd524c82d6991abcc37499ea58a31e98c0a54
Ismail Kılıç <ismail000728@gmail.com>
2022-01-04 23:40:29 +0300

    Active class added for sidebar item.

--- components/Chats/ChatListItem.js
+++ components/Chats/ChatListItem.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { useSetRecoilState } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { currentChatState } from '../../atoms'
 import { UserIcon } from '../Icons'
 
 const ChatListItem = ({ data }) => {
     const setCurrentChat = useSetRecoilState(currentChatState)
+    const currentChat = useRecoilValue(currentChatState)
 
     return (
-        <div onClick={() => setCurrentChat(data.id)} className='h-[72px] flex space-x-4 hover:bg-gray-100 cursor-pointer'>
+        <div
+            onClick={() => setCurrentChat(data.id)}
+            className={`h-[72px] flex space-x-4 cursor-pointer ${data.id === currentChat ? "bg-gray-300" : "hover:bg-gray-100"}`}>
             <div className='py-2 pl-4 '>
                 <div className='relative w-14 h-14'>
                     <span className='absolute'>
@@ -19,7 +22,7 @@ const ChatListItem = ({ data }) => {
                 </div>
             </div>
 
-            <div className='flex-1 border-b'>
+            <div className={`flex-1 ${data.id !== currentChat && "border-b"}`}>
                 <div className='flex flex-col py-2 pr-4'>
                     <div className='flex justify-between'>
                         <span className='text-lg'>{data.name}</span>
@@ -27,7 +30,7 @@ const ChatListItem = ({ data }) => {
                     </div>
 
                     <div className='text-sm'>
-                        {data.lastChat.slice(0,20)}
+                        {data.lastChat.slice(0, 20)}
                     </div>
                 </div>
             </div>
